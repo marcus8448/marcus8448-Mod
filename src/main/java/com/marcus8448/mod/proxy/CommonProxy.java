bc6cf41558685ae17f50afeefc2df6b90a4d83fe
marcus8448 <marcus2475@outlook.com>
2018-08-14 17:54:09 -0700

    IDK what I was doing earlier

--- src/main/java/com/marcus8448/mod/proxy/CommonProxy.java
+++ src/main/java/com/marcus8448/mod/proxy/CommonProxy.java
@@ -9,21 +9,15 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 @SuppressWarnings("deprecation")
 public class CommonProxy {
-	public void registerItemRenderer(Item item, int meta, String id)
-	{
-		
-		
-	}
-	
-	public String localize(String unlocalized, Object... args) {
-		return I18n.translateToLocalFormatted(unlocalized, args);
-		
-	}
-	
-	public void preInit(FMLPreInitializationEvent e) {
-
-        RegistryHandler.registerEntity();
-
-	}
+    public void registerItemRenderer(Item item, int meta, String id)
+    {
+        
+        
+    }
+    
+    public String localize(String unlocalized, Object... args) {
+        return I18n.translateToLocalFormatted(unlocalized, args);
+        
+    }
 
 }
